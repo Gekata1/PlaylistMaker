f95c79058504fe67cb0e6f09b145c3b3a4931eb5
Gekata1 <gekatanothing@ya.ru>
2024-03-08 11:22:04 +0300

    8

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,22 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1 = findViewById<Button>(R.id.button_search)
-        button1.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "ПОИСК", Toast.LENGTH_SHORT).show()
-            }
-        })
+            val button1 = findViewById<Button>(R.id.button_search)
+            button1.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    Toast.makeText(this@MainActivity, "ПОИСК", Toast.LENGTH_SHORT).show()
+                }
+            })
         val button2 = findViewById<Button>(R.id.button_media)
         button2.setOnClickListener {
             Toast.makeText(this@MainActivity, "МЕДИА", Toast.LENGTH_SHORT).show()
         }
         val button3 = findViewById<Button>(R.id.button_settings)
         button3.setOnClickListener {
-            Toast.makeText(this@MainActivity, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
+            val button3Intent = Intent(this, SetingsActivity::class.java)
+            startActivity(button3Intent)
         }
     }
-}
+    
+    }
+
+
+
 
 
 
